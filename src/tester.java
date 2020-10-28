1f3452889d4dc329209bfe623344b2cbc2ec82e7
Dan DeBlasio <dan@dandeblasio.com>
2020-10-28 09:39:14 -0600

    Maybe?

---
+++ src/tester.java
@@ -0,0 +1,9 @@
+
+
+public class tester {
+  public static void main(String[] args) {
+    BinarySearchTreeMistakes tree1 = new BinarySearchTreeMistakes();
+    tree1.insert("C");
+    tree1.print();
+  }
+}
